f8f7d13ba0a2deef2b76a3560cf0eb353fa9c30e
Nathan <capture12020@gmail.com>
2023-10-02 21:13:18 -0400

    feat(added-footer-and-navbar-components-to-Layout.tsx): implemented layout and navbar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div id="layout" className="flex min-h-screen flex-col bg-background">
-      <h1 className="bg-primary">TODO: Navbar</h1>
+      <Navbar />
       {children}
-      <h1 className="mt-auto bg-secondary">TODO: Footer</h1>
+      <Footer />
     </div>
   );
 }
