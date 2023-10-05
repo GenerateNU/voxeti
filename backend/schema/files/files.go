a054807151d0d7f84e35e7cce8f8b99620df4e29
Alder Whiteford <alderjwhiteford@gmail.com>
2023-10-05 11:30:22 -0700

    style(Formatting-Issue): Formatting

--- backend/schema/files/files.go
+++ backend/schema/files/files.go
@@ -17,7 +17,7 @@ func ValidateSTLFile(file *multipart.FileHeader) *schema.ErrorResponse {
 	src, err := file.Open()
 	if err != nil {
 		errorResponse.Code = 500
-		errorResponse.Message = "Failed to open the STL File!";
+		errorResponse.Message = "Failed to open the STL File!"
 		return errorResponse
 	}
 	defer src.Close()
@@ -27,14 +27,14 @@ func ValidateSTLFile(file *multipart.FileHeader) *schema.ErrorResponse {
 	_, err = src.Read(firstBytes)
 	if err != nil {
 		errorResponse.Code = 500
-		errorResponse.Message =  "Failed to read STL file!";
+		errorResponse.Message = "Failed to read STL file!"
 		return errorResponse
 	}
 
 	// Determine which method to call:
 	if string(firstBytes) == "solid" {
 		// Validate an ASCII file:
-		if err := ValidateASCIISTLFile(src, file.Size); err != nil{
+		if err := ValidateASCIISTLFile(src, file.Size); err != nil {
 			return err
 		}
 	} else {
@@ -52,17 +52,17 @@ func ValidateBinarySTLFile(file multipart.File, size int64) *schema.ErrorRespons
 
 	// Check file size:
 	if size < 84 {
-		errorResponse.Code = 400;
-		errorResponse.Message =  "Binary file is too small!";
-		return errorResponse;
+		errorResponse.Code = 400
+		errorResponse.Message = "Binary file is too small!"
+		return errorResponse
 	}
 
 	// Set current read to the 80th byte:
 	_, err := file.Seek(80, io.SeekStart)
 	if err != nil {
-		errorResponse.Code = 400;
-		errorResponse.Message =  "Unable to read bytes after binary file header!";
-		return errorResponse;
+		errorResponse.Code = 400
+		errorResponse.Message = "Unable to read bytes after binary file header!"
+		return errorResponse
 	}
 
 	// Read the number of triangles in bits:
@@ -70,22 +70,22 @@ func ValidateBinarySTLFile(file multipart.File, size int64) *schema.ErrorRespons
 	_, err = file.Read(triangleBytes)
 	if err != nil {
 		errorResponse.Code = 500
-		errorResponse.Message =  "Failed to read triangle bytes from binary STL file!";
+		errorResponse.Message = "Failed to read triangle bytes from binary STL file!"
 		return errorResponse
 	}
 
 	// Convert bits to little-endian unsigned int32:
 	numberOfTriangles := binary.LittleEndian.Uint32(triangleBytes)
 	computedFileSize := 84 + (numberOfTriangles * 50)
-	
+
 	// Confirm file size matches number of triangles + header:
 	if size != int64(computedFileSize) {
 		errorResponse.Code = 400
-		errorResponse.Message =  "Invalid binary file!";
+		errorResponse.Message = "Invalid binary file!"
 		return errorResponse
 	}
 
-	return nil;
+	return nil
 }
 
 func ValidateASCIISTLFile(file multipart.File, size int64) *schema.ErrorResponse {
@@ -93,9 +93,9 @@ func ValidateASCIISTLFile(file multipart.File, size int64) *schema.ErrorResponse
 
 	// Check file size:
 	if size < 15 {
-		errorResponse.Code = 400;
-		errorResponse.Message =  "ASCII file is too small!";
-		return errorResponse;
+		errorResponse.Code = 400
+		errorResponse.Message = "ASCII file is too small!"
+		return errorResponse
 	}
 
 	scanner := bufio.NewScanner(file)
@@ -104,9 +104,9 @@ func ValidateASCIISTLFile(file multipart.File, size int64) *schema.ErrorResponse
 	for scanner.Scan() {
 		// Check to make sure the end of the file is the true end:
 		if seenEndSolid {
-			errorResponse.Code = 400;
-			errorResponse.Message =  "Invalid STL ASCII File, extra line after endsolid!";
-			return errorResponse;
+			errorResponse.Code = 400
+			errorResponse.Message = "Invalid STL ASCII File, extra line after endsolid!"
+			return errorResponse
 		}
 		// Line 2 must begin with "facet" or "endsolid"
 		if lineNumber == 2 {
@@ -114,9 +114,9 @@ func ValidateASCIISTLFile(file multipart.File, size int64) *schema.ErrorResponse
 				seenEndSolid = true
 				continue
 			} else if !strings.HasPrefix(strings.Trim(scanner.Text(), " "), "facet") {
-				errorResponse.Code = 400;
-				errorResponse.Message =  "Invalid STL ASCII File, line 2 missing facet!";
-				return errorResponse;
+				errorResponse.Code = 400
+				errorResponse.Message = "Invalid STL ASCII File, line 2 missing facet!"
+				return errorResponse
 			}
 		}
 		// Locate end of file:
@@ -129,9 +129,9 @@ func ValidateASCIISTLFile(file multipart.File, size int64) *schema.ErrorResponse
 
 	// Check if the end of the file was found:
 	if !seenEndSolid {
-		errorResponse.Code = 400;
-		errorResponse.Message = "Invalid STL ASCII File, end of file not found!";
-		return errorResponse;
+		errorResponse.Code = 400
+		errorResponse.Message = "Invalid STL ASCII File, end of file not found!"
+		return errorResponse
 	} else {
 		return nil
 	}
