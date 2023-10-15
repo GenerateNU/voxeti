6ecf9c39a96783a066621b5f8dde5a44bc1347f3
jtrrll <jacksonterrill3@gmail.com>
2023-10-15 12:26:48 -0400

    refactor(branding): updated tailwind colors and added placeholder logo

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,8 +1,8 @@
 export default function Index() {
   return (
     <h1 className="text-5xl font-bold text-accent text-center">
-      <span className="text-secondary">V</span>o
-      <span className="text-tertiary">x</span>
+      <span className="text-designer">V</span>o
+      <span className="text-producer">x</span>
       eti
     </h1>
   );
