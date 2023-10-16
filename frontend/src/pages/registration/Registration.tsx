e352100f9feb263cb06d1ee4c18270313542617c
natesawant <natesawant@gmail.com>
2023-10-16 18:16:07 -0400

    feat: Add styling, some registration specific questions

--- frontend/src/pages/registration/Registration.tsx
+++ frontend/src/pages/registration/Registration.tsx
@@ -1,101 +1,188 @@
 import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
-const questions = [
-  {
-    section: "Personal Information",
-    questions: [
-      "What is your full name?",
-      "What is your date of birth?",
-      "What is your email address?",
-    ],
-  },
-  {
-    section: "Education",
-    questions: [
-      "What is your highest level of education?",
-      "Where did you attend school?",
-    ],
-  },
-  {
-    section: "Employment",
-    questions: [
-      "What is your current job title?",
-      "Where do you work?",
-      "How long have you been in this job?",
-    ],
-  },
-  {
-    section: "Interests",
-    questions: [
-      "What are your hobbies and interests?",
-      "Do you have any other skills or talents?",
-    ],
-  },
-  // You can add more sections and questions as needed
-];
+
+// Question being asked
+// TODO somehow allow two questions to be on same line
+type FormQuestion = {
+  prompt: string;
+  key: string;
+  rules?: {};
+  type?: string;
+};
+
+// Each "page" of the form
+type FormSection = {
+  sectionTitle: string;
+  questions: FormQuestion[];
+};
+
+// Parent form type with sections
+type MultiForm = {
+  sections: FormSection[];
+};
+
+// Current questions for user registrations
+const questions: MultiForm = {
+  sections: [
+    {
+      sectionTitle: "Welcome to Voxeti",
+      questions: [
+        {
+          prompt: "Email",
+          key: "email",
+          rules: { required: true },
+          type: "email",
+        },
+        {
+          prompt: "Password",
+          key: "password",
+          rules: { required: true },
+          type: "password",
+        },
+      ],
+    },
+    {
+      sectionTitle: "Tell us about yourself!",
+      questions: [
+        {
+          prompt: "First Name",
+          key: "firstName",
+          rules: { required: true },
+          type: "text",
+        },
+        {
+          prompt: "Last Name",
+          key: "lastName",
+          rules: { required: true },
+          type: "text",
+        },
+        {
+          prompt: "Bio",
+          key: "bio",
+        },
+        {
+          prompt: "Address Line 1",
+          key: "address.line1",
+          rules: { required: true },
+        },
+        {
+          prompt: "Address Line 2",
+          key: "address.line2",
+        },
+        {
+          prompt: "City",
+          key: "address.city",
+          rules: { required: true },
+          type: "text",
+        },
+        {
+          prompt: "State",
+          key: "address.state",
+          rules: { required: true },
+          type: "text",
+        },
+        {
+          prompt: "Zip",
+          key: "address.zipCode",
+          rules: { required: true },
+        },
+      ],
+    },
+    // You can add more sections and questions as needed
+  ],
+};
 const QuestionForm = () => {
   const { control, handleSubmit } = useForm();
   const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
   const onSubmit = (data: any) => {
     console.log(data);
   };
-  const currentSection = questions[currentSectionIndex];
+  const currentSection: FormSection = questions.sections[currentSectionIndex];
   const renderQuestions = () => {
     return (
-      <div>
-        <h2>{currentSection?.section}</h2>
-        {currentSection?.questions.map((question, index) => (
-          <div key={index + question}>
+      <div className=" w-96">
+        <h2 className="flex justify-center items-center text-xl">
+          {currentSection?.sectionTitle}
+        </h2>
+        {currentSection?.questions.map((question, _) => (
+          <div key={question.key}>
             <Controller
-              key={index + question + "cont"}
-              name={`${question}`}
+              key={question.key + "cont"}
+              name={`${question.key}`}
               control={control}
               render={({ field }) => (
-                <div>
-                  <label>{question}</label>
-                  <input {...field} />
+                <div className="flex flex-col">
+                  <label className=" py-1">{question.prompt}</label>
+                  <input
+                    className=" outline outline-1 p-2 rounded-sm"
+                    {...field}
+                    type={question.type}
+                  />
                 </div>
               )}
+              rules={question.rules}
             />
           </div>
         ))}
       </div>
     );
   };
+
+  // Go to next section
   const handleNext = () => {
-    if (currentSectionIndex < questions.length - 1) {
+    if (currentSectionIndex < questions.sections.length - 1) {
       setCurrentSectionIndex(currentSectionIndex + 1);
     }
   };
+
+  // Go to previous section
   const handlePrevious = () => {
     if (currentSectionIndex > 0) {
       setCurrentSectionIndex(currentSectionIndex - 1);
     }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {renderQuestions()}
-      <div className="flex">
-        {currentSectionIndex > 0 && (
-          <button
-            className=" bg-producer"
-            type="button"
-            onClick={handlePrevious}
-          >
-            Previous
-          </button>
+    // Only show appropriate buttons based on section index
+    <div className="flex justify-center items-center py-4">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {renderQuestions()}
+        <div className="">
+          {currentSectionIndex > 0 && (
+            <div className=" float-left py-4">
+              <button
+                className=" bg-primary bg-opacity-5 text-primary rounded-lg p-3"
+                type="button"
+                onClick={handlePrevious}
+              >
+                Previous
+              </button>
+            </div>
+          )}
+          {currentSectionIndex < questions.sections.length - 1 && (
+            <div className=" float-right py-4">
+              <button
+                className=" bg-primary text-background rounded-lg p-3"
+                type="button"
+                onClick={handleNext}
+              >
+                Continue
+              </button>
+            </div>
+          )}
+        </div>
+        ​
+        {currentSectionIndex == questions.sections.length - 1 && (
+          <div className=" float-right py-4">
+            <button
+              className=" bg-primary text-background rounded-lg p-3"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
         )}
-        {currentSectionIndex < questions.length - 1 && (
-          <button className=" bg-designer" type="button" onClick={handleNext}>
-            Next
-          </button>
-        )}
-      </div>
-      ​
-      <button className=" bg-call-to-action" type="submit">
-        submit
-      </button>
-    </form>
+      </form>
+    </div>
   );
 };
 export default QuestionForm;
