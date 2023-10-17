52bafcaaa316336531858896c7ed9f2e74b6f460
Nathan Jung <71521507+capture120@users.noreply.github.com>
2023-10-17 16:15:14 -0400

    Job routes (#12)
    * feat(controller.go,-main.go,-go.mod,-go.sum,-job-routes.go,-job-dao.go,-job-model.go): initial routes for jobs
    
    !!!! Hardcoded uri in main.go for local development, ***** remember to change to .env value ******
    
    * refactor(main.go,-job-dao.go,-job-model.go,-job-routes.go): add job-dao object
    
    temporarily modified main.go to use environmental variables, also added .env to .gitignore
    
    * refactor(job-routes.go,-job-model.go): added routes for retrieving jobs by designer or producer id
    
    removed dao object for now
    
    * refactor(job-model.go,-job-routes.go): seperated router logic from model, added patch route for jobs
    
    * refactor(main.go,-job-model.go): fix go-linting issues
    
    * refactor(job-model.go,-job-routes.go,-controller.go): fixed error messages, general refactorings after merging main, removing references to env, using docker mongodb
    
    * refactor(job-model.go,-transactions.go): abstracted out database calls from model into a transactions file

---
+++ backend/controller/job-routes.go
@@ -0,0 +1,114 @@
+package controller
+
+import (
+	// "fmt"
+	"strconv"
+	"voxeti/backend/schema"
+	"voxeti/backend/schema/job"
+	"voxeti/backend/utilities"
+
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/pterm/pterm"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func RegisterJobHandlers(e *echo.Group, dbClient *mongo.Client, logger *pterm.Logger) {
+	api := e.Group("/jobs")
+
+	api.GET("/:id", func(c echo.Context) error {
+		jobId := c.Param("id")
+		retrievedJob, errorResponse := job.GetJobById(jobId, dbClient)
+
+		if errorResponse != nil {
+			return c.JSON(utilities.CreateErrorResponse(errorResponse.Code, errorResponse.Message));
+		}
+
+		return c.JSON(http.StatusOK, retrievedJob)
+	})
+
+	api.GET("", func(c echo.Context) error {
+		limit := 2 // represents the number of results we want per page
+		designerId := c.QueryParam("designer")
+		producerId := c.QueryParam("producer")
+		page_num, _ := strconv.Atoi(c.QueryParam("page")) // the current page the user is on
+		skip := limit * page_num
+		// fmt.Println(skip);
+		// fmt.Println(page_num);
+		// fmt.Println(designerId);
+		// fmt.Println(producerId);
+		
+		if page_num < 0 {
+			return c.JSON(utilities.CreateErrorResponse(400, "Invalid page number"))
+		}
+		retrievedJobs, errorResponse := job.GetJobsByDesignerOrProducerId(designerId, producerId, int64(limit), int64(skip), dbClient)
+		if errorResponse != nil {
+			return c.JSON(utilities.CreateErrorResponse(errorResponse.Code, errorResponse.Message))
+		}
+
+		return c.JSON(http.StatusOK, retrievedJobs)
+	})
+
+	api.DELETE("/:id", func(c echo.Context) error {
+		// get job ID
+		jobIDStr := c.Param("id")
+		errorResponse := job.DeleteJob(jobIDStr, dbClient)
+		if errorResponse != nil {
+			return c.JSON(utilities.CreateErrorResponse(errorResponse.Code, errorResponse.Message))
+		}
+
+		return c.NoContent(http.StatusOK)
+	})
+
+	api.POST("", func(c echo.Context) error {
+		// create new Job with given data
+		newJob := new(schema.Job)
+		if err := c.Bind(newJob); err != nil {
+			return c.JSON(utilities.CreateErrorResponse(400, "Invalid job data"))
+		}
+		jobCreated, errorResponse := job.CreateJob(*newJob, dbClient)
+
+		if errorResponse != nil {
+			return c.JSON(utilities.CreateErrorResponse(errorResponse.Code, errorResponse.Message))
+		}
+
+		return c.JSON(http.StatusOK, jobCreated)
+	})
+
+	api.PUT("/:id", func(c echo.Context) error {
+		// get job ID
+		jobId := c.Param("id")
+		job_body_param := new(schema.Job)
+		if err := c.Bind(job_body_param); err != nil {
+			return c.JSON(utilities.CreateErrorResponse(400, "Invalid job data"))
+		}
+		retrievedJob, errorResponse := job.UpdateJob(jobId, *job_body_param, dbClient)
+
+		if errorResponse != nil {
+			return c.JSON(utilities.CreateErrorResponse(errorResponse.Code, errorResponse.Message))
+		}
+
+		return c.JSON(http.StatusOK, retrievedJob)
+	})
+
+	api.PATCH("/:id", func(c echo.Context) error {
+		jobIdStr := c.Param("id")
+		jobId, err := primitive.ObjectIDFromHex(jobIdStr)
+		if err != nil {
+			return c.JSON(utilities.CreateErrorResponse(400, "Invalid job ID"))
+		}
+		patchData := bson.M{}
+		if err := c.Bind(&patchData); err != nil {
+			return c.JSON(utilities.CreateErrorResponse(400, "Invalid patch data"))
+		}
+		patchedJob, errorResponse := job.PatchJob(jobId, patchData, dbClient)
+		if errorResponse != nil {
+			return c.JSON(utilities.CreateErrorResponse(errorResponse.Code, errorResponse.Message))
+		}
+		return c.JSON(http.StatusOK, patchedJob)
+	})
+	
+}
