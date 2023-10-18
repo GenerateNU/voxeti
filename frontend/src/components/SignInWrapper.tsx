111b454d9157cf88345303002999bc42015114f8
Nathan <capture12020@gmail.com>
2023-10-17 20:14:59 -0400

    refactor(SignInWrapper.tsx): trying out a basic signin page

--- frontend/src/components/SignInWrapper.tsx
+++ frontend/src/components/SignInWrapper.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
@@ -8,7 +7,6 @@ import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
@@ -56,17 +54,18 @@ export default function SignIn() {
             alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center", justifyContent: "flex-start" }}>
+            <img src="/favicon.ico" alt="Profile Picture" />
+            <Typography component="h1" variant="h5" sx={{ textAlign: "left", fontSize: "2rem" }}>
+              Sign in
+            </Typography>
+          </Box>
           <Box
             component="form"
             onSubmit={handleSubmit}
             noValidate
             sx={{ mt: 1 }}
+            border-radius="50px"
           >
             <TextField
               margin="normal"
@@ -77,6 +76,11 @@ export default function SignIn() {
               name="email"
               autoComplete="email"
               autoFocus
+              InputProps={{
+                style: {
+                  borderRadius: "10px",
+                },
+              }}
             />
             <TextField
               margin="normal"
@@ -87,28 +91,40 @@ export default function SignIn() {
               type="password"
               id="password"
               autoComplete="current-password"
+              InputProps={{
+                style: {
+                  borderRadius: "10px",
+                },
+              }}
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <FormControlLabel
+                  required
+                  control={<Checkbox />}
+                  label="Required"
+                  sx={{ py: 1 }}
+                />
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" sx={{ py: 1, color: 'black', textDecoration: 'none' }}>
+                  {"Forgot password?"}
+                </Link>
+              </Grid>
+            </Grid>
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              color="inherit"
             >
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                <Link href="#" variant="body2" sx={{ py: 1, color: 'black', textDecoration: 'none' }}>
+                  {"Don't have an account?"}
                 </Link>
               </Grid>
             </Grid>
