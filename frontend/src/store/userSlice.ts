7cfb2981318e2669de504900d037eb322e39bef0
aaronkimbrooktec <218adk@gmail.com>
2023-10-19 14:45:29 -0400

    refactor(userSlice.ts): remove unnecessary reducers

--- frontend/src/store/userSlice.ts
+++ frontend/src/store/userSlice.ts
@@ -1,6 +1,5 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { UserSliceState } from "./store.types";
-import { Address, ExperienceLevel, Printer, Filament } from "../main.types";
 
 // Base user state:
 const initialState : UserSliceState = {
@@ -37,38 +36,13 @@ export const userSlice = createSlice({
 
     // 2. Reset user state on logout or session revocation:
     resetUser: () => initialState,
-
-    // 3. Registration reducers:
-    setCredentials: (state, action: PayloadAction<{ email: string, password: string }>) => {
-      state.user.email = action.payload.email;
-      state.user.password = action.payload.password;
-    },
-
-    setPersonalInfo: (state, action: PayloadAction<{ firstName: string, lastName: string, address: Address }>) => {
-      state.user.firstName = action.payload.firstName;
-      state.user.lastName = action.payload.lastName;
-      state.user.addresses.push(action.payload.address);
-    },
-
-    setExperience: (state, action: PayloadAction<ExperienceLevel>) => {
-      state.user.experience = action.payload;
-    },
-
-    setProducer: (state, action: PayloadAction<{ printer: Printer, filament: Filament }>) => {
-      state.user.printers.push(action.payload.printer);
-      state.user.availableFilament.push(action.payload.filament);
-    },
   }
 })
 
 // Export reducers for use:
 export const {
   setUser,
-  resetUser,
-  setCredentials,
-  setPersonalInfo,
-  setExperience,
-  setProducer,
+  resetUser
 } = userSlice.actions;
 
 export default userSlice.reducer;
