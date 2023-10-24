54199ae750f7e06ff3f8df2b666989a74b2ba075
Alder Whiteford <alderjwhiteford@gmail.com>
2023-10-24 15:42:08 -0400

    style(Google-SSO): Styling Fixes

--- backend/controller/auth.go
+++ backend/controller/auth.go
@@ -1,58 +1,58 @@
 package controller
 
 import (
-  "net/http"
-  "voxeti/backend/schema"
-  "voxeti/backend/schema/auth"
-  "voxeti/backend/utilities"
+	"net/http"
+	"voxeti/backend/schema"
+	"voxeti/backend/schema/auth"
+	"voxeti/backend/utilities"
 
-  "github.com/gorilla/sessions"
-  "github.com/labstack/echo/v4"
-  "github.com/pterm/pterm"
-  "go.mongodb.org/mongo-driver/mongo"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo/v4"
+	"github.com/pterm/pterm"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func RegisterAuthHandlers(e *echo.Group, store *sessions.CookieStore, dbClient *mongo.Client, logger *pterm.Logger) {
-  api := e.Group("/auth")
+	api := e.Group("/auth")
 
-  api.POST("/login", func(c echo.Context) error {
-    var creds schema.Credentials
+	api.POST("/login", func(c echo.Context) error {
+		var creds schema.Credentials
 
-    if err := c.Bind(&creds); err != nil {
-      return c.JSON(utilities.CreateErrorResponse(400, "Failed to unmarshal credentials"))
-    }
+		if err := c.Bind(&creds); err != nil {
+			return c.JSON(utilities.CreateErrorResponse(400, "Failed to unmarshal credentials"))
+		}
 
-    response, err := auth.Login(c, store, dbClient, creds)
-    if err != nil {
-      return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
-    }
+		response, err := auth.Login(c, store, dbClient, creds)
+		if err != nil {
+			return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
+		}
 
-    return c.JSON(http.StatusOK, response)
-  })
+		return c.JSON(http.StatusOK, response)
+	})
 
-  api.POST("/logout", func(c echo.Context) error {
-    if err := auth.AuthenticateSession(c, store); err != nil {
-      return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
-    }
+	api.POST("/logout", func(c echo.Context) error {
+		if err := auth.AuthenticateSession(c, store); err != nil {
+			return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
+		}
 
-    if err := auth.InvalidateUserSession(c, store); err != nil {
-      return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
-    }
-    return c.NoContent(http.StatusOK)
-  })
+		if err := auth.InvalidateUserSession(c, store); err != nil {
+			return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
+		}
+		return c.NoContent(http.StatusOK)
+	})
 
-  api.POST("/google-provider", func(c echo.Context) error {
-    var accessToken schema.GoogleAccessToken
+	api.POST("/google-provider", func(c echo.Context) error {
+		var accessToken schema.GoogleAccessToken
 
-    if err := c.Bind(&accessToken); err != nil {
-      return c.JSON(utilities.CreateErrorResponse(400, "Failed to unmarshal accessToken"))
-    }
+		if err := c.Bind(&accessToken); err != nil {
+			return c.JSON(utilities.CreateErrorResponse(400, "Failed to unmarshal accessToken"))
+		}
 
-    googleSSOUser, err := auth.GoogleSSOAuthentication(c, store, accessToken, dbClient)
-    if err != nil {
-      return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
-    }
+		googleSSOUser, err := auth.GoogleSSOAuthentication(c, store, accessToken, dbClient)
+		if err != nil {
+			return c.JSON(utilities.CreateErrorResponse(err.Code, err.Message))
+		}
 
-    return c.JSON(http.StatusOK, googleSSOUser)
-  })
+		return c.JSON(http.StatusOK, googleSSOUser)
+	})
 }
