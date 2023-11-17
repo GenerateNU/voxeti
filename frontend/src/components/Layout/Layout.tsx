35aaec1e4ad0e1ad85d18a265ae6b9a2942eee3d
Alder Whiteford <alderjwhiteford@gmail.com>
2023-11-16 21:16:12 -0500

    Updated Navbar (#26)
    * Updated Navbar
    
    * Fix Naming Conventions

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../Footer/Footer";
-import Navbar from "../Navbar/Navbar";
+// import Navbar from "../Navbar/Navbar";
+import NavBar from "../Navbar/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,7 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       id="layout"
       className="min-h-screen flex flex-col bg-background text-body-text"
     >
-      <Navbar />
+      <NavBar />
       {children}
       <Footer />
     </div>
