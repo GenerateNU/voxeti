ee264d89f1aca7794bea6df44660b89bf6b91e1e
Nate Sawant <natesawant@gmail.com>
2023-11-30 12:57:49 -0500

    cleanup designer info

--- frontend/src/components/JobAccept/DesignerInfo.tsx
+++ frontend/src/components/JobAccept/DesignerInfo.tsx
@@ -1,29 +1,17 @@
 import { userApi } from "../../api/api";
-import { Address, Job } from "../../main.types";
+import { Job } from "../../main.types";
 import { Avatar } from "@mui/material";
-import Divider from "@mui/material/Divider";
-import * as React from "react";
 import JobAcceptButtons from "./JobAcceptButtons";
 
 export default function DesignerName(props: { job: Job }) {
   const { data: data } = userApi.useGetUserQuery(props.job.designerId);
-  const [address, setAddress] = React.useState<Address>();
-
-  React.useEffect(() => {
-    if (data) {
-      // Need to figure out which address to use for shipping
-      const shipping = data.addresses[0];
-      setAddress(shipping);
-    }
-  }, [data]);
-
   return (
     <div className=" flex flex-col w-full">
       <div className=" flex flex-row items-center justify-between w-full">
         <div className=" flex flex-row">
           <Avatar
             className=" outline outline-3 outline-offset-2 outline-designer"
-            alt="Remy Sharp"
+            alt={data ? `${data.firstName} ${data.lastName}` : ""}
             src="/static/images/avatar/1.jpg"
             sx={{ width: 64, height: 64 }}
           />
