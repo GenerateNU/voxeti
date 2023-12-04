28cab0564dd902956d24994eb7c7c26ceb9a07fc
Nate Sawant <natesawant@gmail.com>
2023-12-03 20:52:07 -0500

    Add skeleton placeholders

--- frontend/src/components/Jobs/ProducerJobs/components/DesignerInfo.tsx
+++ frontend/src/components/Jobs/ProducerJobs/components/DesignerInfo.tsx
@@ -2,6 +2,7 @@ import { userApi } from "../../../../api/api";
 import { Job } from "../../../../main.types";
 import { Avatar } from "@mui/material";
 import JobAcceptButtons from "./JobAcceptButtons";
+import { Skeleton } from "@mui/material";
 
 function capitalize(str?: string) {
   return str ? str[0].toUpperCase() + str.slice(1).toLowerCase() : "";
@@ -13,24 +14,33 @@ export default function DesignerName(props: { designerId: string; job?: Job }) {
     <div className=" flex flex-col w-full">
       <div className=" flex flex-row items-center justify-between w-full">
         <div className=" flex flex-row">
-          <Avatar
-            className={` outline outline-3 outline-offset-2 ${
-              data?.userType == "DESIGNER"
-                ? "outline-designer"
-                : "outline-producer"
-            }`}
-            alt={data ? `${data.firstName} ${data.lastName}` : ""}
-            src="/static/images/avatar/1.jpg"
-            sx={{ width: 64, height: 64 }}
-          />
-          <div className=" px-4 flex flex-col justify-center">
-            <p className=" text-lg">
-              {data && data.firstName} {data && data.lastName}
-            </p>
-            <p className=" text-sm opacity-70">
-              {data && capitalize(data.userType)}
-            </p>
-          </div>
+          {data ? (
+            <Avatar
+              className={` outline outline-3 outline-offset-2 ${
+                data.userType == "DESIGNER"
+                  ? "outline-designer"
+                  : "outline-producer"
+              }`}
+              alt={`${data.firstName} ${data.lastName}`}
+              src="/static/images/avatar/1.jpg"
+              sx={{ width: 64, height: 64 }}
+            />
+          ) : (
+            <Skeleton variant="circular" width={64} height={64} />
+          )}
+          {data ? (
+            <div className=" px-4 flex flex-col justify-center">
+              <p className=" text-lg">
+                {data && data.firstName} {data && data.lastName}
+              </p>
+              <p className=" text-sm opacity-70">{capitalize(data.userType)}</p>
+            </div>
+          ) : (
+            <div className=" px-4 flex flex-col justify-center">
+              <Skeleton variant="rectangular" width={96} height={28} />
+              <Skeleton variant="rectangular" width={48} height={20} />
+            </div>
+          )}
         </div>
         {props.job && <JobAcceptButtons currentJob={props.job} />}
       </div>
